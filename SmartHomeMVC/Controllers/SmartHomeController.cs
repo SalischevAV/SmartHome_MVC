07ca1265904b0897fff0ba0f1186abcb08e85155
SalischevAV <devorer77@gmail.com>
2017-09-03 21:47:39 +0300

    Добавьте файлы проекта.

---
+++ SmartHomeMVC/Controllers/SmartHomeController.cs
@@ -0,0 +1,45 @@
+﻿using NewSmartHome.DeviceClasses;
+using NewSmartHome.Interfaces;
+using NewSmartHome.ServiceClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartHomeMVC.Controllers
+{
+    public class SmartHomeController : Controller
+    {
+        // GET: SmartHome
+        public ActionResult Index()
+        {
+            Dictionary<string, Device> smartHoseDevicesDictionary = new Dictionary<string, Device>();
+            DeviceCreator myDCreator = new DeviceCreator();
+
+            smartHoseDevicesDictionary.Add("Nord", myDCreator.CreateDevice("fridge"));
+
+            smartHoseDevicesDictionary.Add("Mitsubishi", myDCreator.CreateDevice("conditioner"));
+
+            smartHoseDevicesDictionary.Add("Spidola", myDCreator.CreateDevice("radio"));
+
+            smartHoseDevicesDictionary.Add("Indesit", myDCreator.CreateDevice("oven"));
+
+            smartHoseDevicesDictionary.Add("Siemens", myDCreator.CreateDevice("mwoven"));
+
+            smartHoseDevicesDictionary.Add("Seiko", myDCreator.CreateDevice("radiolamp"));
+
+
+            Fridge f = new Fridge();
+            if (f is IModeable)
+            {
+                f = null;
+            }
+
+
+            Session["Devices"] = smartHoseDevicesDictionary;
+            return View(smartHoseDevicesDictionary);
+
+        }
+    }
+}
